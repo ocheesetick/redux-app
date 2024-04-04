ab6bd7ae04c925aaddd8d69be08d1391ec732f7f
JT Rael <raeljohntimothy@gmail.com>
2024-04-04 18:56:52 +0800

    Initial structure

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,26 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
+import { Pressable, StyleSheet } from 'react-native';
 import { Text, View } from '@/components/Themed';
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Text style={styles.title}>Count: </Text>
+      <View style={{flexDirection: "row", gap: 30}}>
+        <Pressable 
+          style={{backgroundColor: "black", padding: 20, borderRadius: 15}}
+        >
+            <Text style={{fontSize: 25, fontWeight: "600", color: "white"}}>
+              +
+            </Text>
+        </Pressable>
+        <Pressable 
+          style={{backgroundColor: "black", padding: 20, borderRadius: 15}}
+        >
+            <Text style={{fontSize: 25, fontWeight: "600", color: "white" }}>
+              -
+            </Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
@@ -18,9 +30,10 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 20
   },
   title: {
-    fontSize: 20,
+    fontSize: 30,
     fontWeight: 'bold',
   },
   separator: {
